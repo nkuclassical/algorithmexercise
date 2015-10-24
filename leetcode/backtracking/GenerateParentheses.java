8ae5c37784fd59184431a406e3a8d7ce5c431c7d
Yanqiao Zhan <zhanyanqiao@gmail.com>
2015-10-24 16:07:13 -0400

    Improve solution to generate parentheses

--- leetcode/backtracking/GenerateParentheses.java
+++ leetcode/backtracking/GenerateParentheses.java
@@ -4,28 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GenerateParentheses {
-	public List<String> generateParenthese(int n) {
-		List<String>ret=new ArrayList();
-		generate(ret,"",n,n);
-		return ret;
-	}
-	
-	public void generate(List<String>ret,String s, int left, int right){
-		if(left==0&&right==0){
-			ret.add(new String(s));
-			return;
-		}
-		if(left>right)return;
-		if(left>0)
-			generate(ret,s+'(',left-1,right);
-		if(right>0)
-			generate(ret,s+')',left, right-1);
-		
-	}
-	
+	public List<String> generateParenthesis(int n) {
+        List<String>ret=new ArrayList();
+        helper(ret,new StringBuilder(),n,n);
+        return ret;
+    }
+    public void helper(List<String>ret, StringBuilder sb, int left, int right){
+        if(left==0&&right==0){
+            ret.add(sb.toString());
+        }else{
+            if(left>right)return;
+            if(left>0){
+                helper(ret,sb.append("("),left-1,right);
+                sb.deleteCharAt(sb.length()-1);
+            }
+            if(right>0){
+                helper(ret,sb.append(")"),left,right-1);
+                sb.deleteCharAt(sb.length()-1);
+            }
+        }
+    }
 	public static void main(String[]args){
 		GenerateParentheses test=new GenerateParentheses();
-		List<String>result=test.generateParenthese(10);
+		List<String>result=test.generateParenthesis(10);
 		for(String r:result)
 			System.out.println(r);
 	}
