ab93efc27a5dba3a2d5eb52667591ad04c162fdc
Yanqiao Zhan <zhanyanqiao@gmail.com>
2015-10-24 17:56:17 -0400

    update major element

--- leetcode/binarysearch/MajorityElement.java
+++ leetcode/binarysearch/MajorityElement.java
@@ -7,37 +7,19 @@ import java.util.LinkedList;
 
 public class MajorityElement {
 	public int majorityElement(int[] nums) {
-		int m = nums[0];
-		int c = 1;
+		int ret = nums[0];
+		int times = 1;
 		for (int i = 1; i < nums.length; i++) {
-			if (nums[i] == m) {
-				c++;
-			} else if (c > 0)
-				c--;
-			else {
-				m = nums[i];
-				c = 1;
-			}
-		}
-		return m;
-	}
-
-	public int majorityElement2(int[] nums) {
-		if (nums == null || nums.length == 0)
-			return -1;
-		if (nums.length == 1)
-			return nums[0];
-		HashMap<Integer, Integer> hashtable = new HashMap();
-		for (int num : nums) {
-			if (hashtable.containsKey(num)) {
-				hashtable.put(num, hashtable.get(num) + 1);
-				if (hashtable.get(num) > nums.length / 2)
-					return num;
+			if (nums[i] == ret) {
+				times++;
+			} else if (times == 0) {
+				ret = nums[i];
+				times = 1;
 			} else {
-				hashtable.put(num, 1);
+				times--;
 			}
 		}
-		return -1;
+		return ret;
 	}
 
 	public static void main(String[] args) {
@@ -45,8 +27,6 @@ public class MajorityElement {
 		nums[0] = 6;
 		nums[1] = 5;
 		nums[2] = 5;
-		// nums[3] = 1 ;
-		// nums[4] = 6;
 
 		MajorityElement test = new MajorityElement();
 		System.out.println(test.majorityElement(nums));
