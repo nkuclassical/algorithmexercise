396ff720038984c551c5f6fbdd73468c811be828
Yanqiao Zhan <zhanyanqiao@gmail.com>
2015-10-28 18:14:46 -0400

    Remove useless code

--- leetcode/arraylist/PascalsTriangleII.java
+++ leetcode/arraylist/PascalsTriangleII.java
@@ -4,45 +4,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class PascalsTriangleII {
-	public long p(int m) {
-		long ret = 1;
-		for (int i = 2; i <= m; i++) {
-			ret *= i;
-		}
-		return ret;
-	}
-
-	public int c(int n, int m) {
-		long ret = 1;
-		for (int i = m - n + 1; i <= m; i++) {
-			ret *= i;
-		}
-		return (int) (ret / p(n));
-	}
-
-	public List<Integer> getRow(int rowIndex) {
-		HashMap<Integer, Integer> map = new HashMap();
-		List<Integer> ret = new ArrayList();
-		for (int i = 0; i <= rowIndex; i++) {
-			if (map.containsKey(rowIndex - i)) {
-				ret.add(map.get(rowIndex - i));
-				continue;
-			}
-			int temp=c(i,rowIndex);
-			ret.add(temp);
-			map.put(i, temp);
-		}
-		return ret;
-	}
-	
+public class PascalsTriangleII {	
 	public List<Integer>getRow2(int rowIndex){
 		List<Integer>ret=new ArrayList();
 		ret.add(1);
 		long previous=1;
 		for(int i=1;i<=rowIndex;i++){
 			previous=previous*(rowIndex-i+1)/i;
-			ret.add(i,(int)previous);
+			ret.add((int)previous);
 		}
 		return ret;
 	}
