b14aa504357ab69d775b110b62b586153ef83bde
Yanqiao Zhan <zhanyanqiao@gmail.com>
2015-10-29 19:35:45 -0400

    delete useless import

--- leetcode/hashtable/GroupAnagrams.java
+++ leetcode/hashtable/GroupAnagrams.java
@@ -3,7 +3,6 @@ package hashtable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
