636ec181b1a2f5b69d3ab39b0c7204d5c4a95f20
Yanqiao Zhan <zhanyanqiao@gmail.com>
2015-10-29 11:16:33 -0400

    rewrite

--- leetcode/tree/ClosestBinarySearchTreeValueII.java
+++ leetcode/tree/ClosestBinarySearchTreeValueII.java
@@ -1,86 +1,34 @@
 package tree;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
 public class ClosestBinarySearchTreeValueII {
-	public List<Integer> cloestKValues(TreeNode root, double target, int k) {
-		Stack<TreeNode> bigger = new Stack();
-		Stack<TreeNode> smaller = new Stack();
-		TreeNode node = root;
-		List<Integer> ret = new ArrayList();
-		while (node != null) {
-			if (node.val > target) {
-				bigger.push(node);
-				node = node.left;
-			} else if (node.val <= target) {
-				smaller.push(node);
-				node = node.right;
-			}
-		}
-
-		while (ret.size() < k) {
-			if (smaller.isEmpty()
-					|| (!bigger.isEmpty() && (bigger.peek().val - target < target
-							- smaller.peek().val))) {
-				TreeNode cur = bigger.pop();
-				ret.add(cur.val);
-				cur = cur.right;
-				while (cur != null) {
-					bigger.push(cur);
-					cur = cur.left;
-				}
-			} else {
-				TreeNode cur = smaller.pop();
-				ret.add(cur.val);
-				cur = cur.left;
-				while (cur != null) {
-					smaller.push(cur);
-					cur = cur.right;
-				}
-			}
-		}
-		return ret;
-	}
-
-	public List<Integer> closestKValues(TreeNode root, double target, int k) {
-		Stack<TreeNode> bigger = new Stack();
-		Stack<TreeNode> smaller = new Stack();
+	public List<Integer> closestKValues2(TreeNode root, double target, int k) {
+		Stack<TreeNode> stack = new Stack();
+		LinkedList<Integer> ret = new LinkedList();
 		TreeNode cur = root;
-		while (cur != null) {
-			if (cur.val < target) {
-				smaller.push(cur);
-				cur = cur.right;
-			} else {
-				bigger.push(cur);
+		while (cur != null || !stack.isEmpty()) {
+			if (cur != null) {
+				stack.push(cur);
 				cur = cur.left;
+			} else {
+				cur = stack.pop();
+				if (ret.size() < k) {
+					ret.add(cur.val);
+				} else {
+					if (Math.abs(ret.getFirst() - target) > Math.abs(cur.val
+							- target)) {
+						ret.removeFirst();
+						ret.add(cur.val);
+					} else
+						break;
+				}
+				cur = cur.right;
 			}
 		}
-
-		List<Integer> ret = new ArrayList();
-		for (int i = 0; i < k; i++) {
-			if (bigger.isEmpty()
-					|| (!smaller.isEmpty() && (bigger.peek().val - target > target
-							- smaller.peek().val))) {
-				cur = smaller.pop();
-				ret.add(cur.val);
-				cur = cur.left;
-				while (cur != null) {
-					smaller.push(cur);
-					cur = cur.right;
-				}
-			} else {
-				cur = bigger.pop();
-				ret.add(cur.val);
-				cur = cur.right;
-				while (cur != null) {
-					bigger.push(cur);
-					cur = cur.left;
-				}
-			}
-		}
-
 		return ret;
 	}
 }
