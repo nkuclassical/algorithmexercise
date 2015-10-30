8f8ca05c1e97f910360490996db524c126d2595c
Yanqiao Zhan <zhanyanqiao@gmail.com>
2015-10-29 21:21:35 -0400

    rewrite

--- leetcode/binarysearch/FindMinimuminRotatedSortedArrayII.java
+++ leetcode/binarysearch/FindMinimuminRotatedSortedArrayII.java
@@ -1,31 +1,26 @@
 package binarysearch;
 
-public class FindMinimuminRotatedSortedArrayII {
+public class FindMinimuminRotatedSortedArrayII {	
 	public int findMin(int[] nums) {
-		if (nums == null || nums.length == 0)
-			return Integer.MAX_VALUE;
-		int start = 0;
-		int end = nums.length - 1;
-		return find(nums, start, end);
+        int start=0, end=nums.length-1;
+        while(start<end){
+        	while(start<end&&nums[start]==nums[end])start++;
+        	int mid=start+(end-start)/2;
+            if(mid-1>=0&&nums[mid-1]>nums[mid])return nums[mid];
+            if(mid+1<nums.length&&nums[mid]>nums[mid+1])return nums[mid+1];
+            if(nums[mid]<=nums[end]){
+                end=mid;
+            }else if(nums[mid]>=nums[start]){
+                start=mid;
+            }
+        }
+        return nums[start];
+    }
+	
+	public static void main(String[]args){
+		FindMinimuminRotatedSortedArrayII test=new FindMinimuminRotatedSortedArrayII();
+		int[]nums={1,1,3,1};
+		System.out.println(test.findMin(nums));
 	}
-
-	public int find(int[] nums, int start, int end) {
-		if (start == end - 1)
-			return Math.min(nums[start], nums[end]);
-		if(start>end)
-			return nums[0];
-		int mid = (start + end) / 2;
-		if (mid > 0 && nums[mid - 1] > nums[mid])
-			return nums[mid];
-		if (mid + 1 <= end && nums[mid] > nums[mid + 1])
-			return nums[mid + 1];
-		if (nums[start] == nums[end] && nums[mid] == nums[start]) {
-			return Math.min(find(nums, start, mid - 1),
-					find(nums, mid + 1, end));
-		}
-		if (nums[start] <= nums[mid]) {
-			return find(nums, mid + 1, end);
-		} else
-			return find(nums, start, mid - 1);
-	}
+	
 }
