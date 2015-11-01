09ff06700a39ee52eafd826fac55c0a26fb3abdc
Yanqiao Zhan <zhanyanqiao@gmail.com>
2015-11-01 17:04:38 -0500

    WordBreakII

--- leetcode/dynamicprogramming/WordBreakII.java
+++ leetcode/dynamicprogramming/WordBreakII.java
@@ -11,11 +11,6 @@ public class WordBreakII {
 
 	Map<String, List<String>> results = new HashMap();
 
-	// public List<String> wordBreak(String s, Set<String> wordDict) {
-	// return helper(s, wordDict);
-	//
-	// }
-
 	public List<String> wordBreak(String s, Set<String> wordDict) {
 		List<String> ret = new ArrayList();
 		for (int i = 1; i <= s.length(); i++) {
@@ -39,34 +34,6 @@ public class WordBreakII {
 		return ret;
 	}
 
-	// Map<String, List<String>> results = new HashMap<String, List<String>>();
-	//
-	// public List<String> wordBreak(String s, Set<String> dict) {
-	// List<String> words = new ArrayList<String>();
-	//
-	// int len = s.length();
-	// for (int i = 1; i <= len; i++) {
-	// String front = s.substring(0, i);
-	// if (dict.contains(front)) {
-	// if (i == len) {
-	// words.add(front);
-	// } else {
-	// String remain = s.substring(i, len);
-	// List<String> remainSet = results.containsKey(remain) ? results
-	// .get(remain) : wordBreak(remain, dict);
-	// if (remainSet != null) {
-	// for (String item : remainSet) {
-	// words.add(front + " " + item);
-	// }
-	// results.put(remain, remainSet);
-	// }
-	//
-	// }
-	// }
-	// }
-	// return words;
-	// }
-
 	public static void main(String[] args) {
 		WordBreakII test = new WordBreakII();
 		Set<String> set = new HashSet();
@@ -75,7 +42,6 @@ public class WordBreakII {
 		set.add("and");
 		set.add("sand");
 		set.add("dog");
-		// set.add("cat");
 		List<String> result = test.wordBreak("catsanddog", set);
 		for (String rt : result) {
 			System.out.println(rt);
