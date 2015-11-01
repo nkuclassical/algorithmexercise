aadcd03a77ed749a69af3449d264bde9698f6726
Yanqiao Zhan <zhanyanqiao@gmail.com>
2015-11-01 17:22:13 -0500

    rewrite

--- leetcode/graph/CloneGraph.java
+++ leetcode/graph/CloneGraph.java
@@ -3,27 +3,20 @@ package graph;
 import java.util.HashMap;
 import java.util.Map;
 
-public class CloneGraph {
-	public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
-		HashMap<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap();
-		return subClone(node, map);
-	}
-
-	public UndirectedGraphNode subClone(UndirectedGraphNode node,
-			HashMap<UndirectedGraphNode, UndirectedGraphNode> map) {
-		if (node == null)
-			return null;
-		UndirectedGraphNode newnode = new UndirectedGraphNode(node.label);
-		map.put(node, newnode);
-		for (UndirectedGraphNode neighbor : node.neighbors) {
-			UndirectedGraphNode newneighbor;
-			if (map.containsKey(neighbor)) {
-				newneighbor = map.get(neighbor);
-			} else {
-				newneighbor = subClone(neighbor, map);
-			}
-			newnode.neighbors.add(newneighbor);
-		}
-		return newnode;
-	}
+public class CloneGraph {	
+	Map<UndirectedGraphNode,UndirectedGraphNode>map=new HashMap();
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        UndirectedGraphNode newnode=new UndirectedGraphNode(node.label);
+        map.put(node,newnode);
+        for(UndirectedGraphNode neighbor:node.neighbors){
+            UndirectedGraphNode newneighbor=null;
+            if(map.containsKey(neighbor)){
+                newneighbor=map.get(neighbor);
+            }else{
+            	newneighbor=cloneGraph(neighbor);
+            }
+            newnode.neighbors.add(newneighbor);
+        }
+        return newnode;
+    }
 }
